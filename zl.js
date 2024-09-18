226187c3e8bd174bf770b317a7d7d407efcb9289
MR-Alastor <96520779+MR-Alastor@users.noreply.github.com>
2024-09-18 23:32:09 +0300

    Update zl.js

--- zl.js
+++ zl.js
@@ -10,28 +10,24 @@ window.onload = function() {
   };
 };
 
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$('body').append('<div class="upbtn"></div>');            
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('.upbtn').css({
+            right: '-120px',
+            bottom: '-120px'
+        });
+        } else {
+        $('.upbtn').css({
+            right: '-220px',
+            bottom: '-220px'
+        });
+    }
+});
+$('.upbtn').on('click',function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
