7f478971422292582ebbf3b0db8d17623a197675
MR-Alastor <96520779+MR-Alastor@users.noreply.github.com>
2024-09-18 20:06:00 +0300

    Create zl.js

---
+++ zl.js
@@ -0,0 +1,12 @@
+
+window.onload = function() {
+  document.forms.formText.onclick = function() {
+    var sum = 0,
+      els = this.querySelectorAll('input[type="checkbox"]:checked');
+    for (var i = 0; i < els.length; i++) {
+      sum += +els[i].value;
+    }
+    this.sumOut.value = sum;
+  };
+};
+
