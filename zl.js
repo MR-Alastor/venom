1e4b44db83b9d440d7649c74df8d87da6be811c5
MR-Alastor <96520779+MR-Alastor@users.noreply.github.com>
2024-09-19 01:53:21 +0300

    Update zl.js

--- zl.js
+++ zl.js
@@ -11,13 +11,3 @@ window.onload = function() {
 };
 
 //-----------------------------------------------
-jQuery(($) => {
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 780) $(".to-up").fadeIn();
-        else $(".to-up").fadeOut();
-    });
-    $(".to-up").click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 566);
-        return false;
-    });
-});
